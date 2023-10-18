1e016845e53ba3e851318ebf017adb703f27beaf
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2023-10-18 22:44:03 +0700

    Showing a Date Picker

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -10,6 +10,18 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate =
+        DateTime(now.year - 1, now.month - 1, now.day - 1, now.day);
+
+    showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -35,14 +47,34 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: InputDecoration(label: Text("Title")),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            maxLength: 50,
-            decoration:
-                InputDecoration(prefixText: "\$", label: Text("Amount")),
-          ),
+          Row(children: [
+            Expanded(
+              child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                maxLength: 50,
+                decoration:
+                    InputDecoration(prefixText: "\$", label: Text("Amount")),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Selected date"),
+                  IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: Icon(Icons.calendar_month))
+                ],
+              ),
+            )
+          ]),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               TextButton(
                   style: ButtonStyle(
@@ -59,8 +91,7 @@ class _NewExpenseState extends State<NewExpense> {
                   child: Text("Save Expense")),
               TextButton(
                   onPressed: () {
-                    print(_titleController.text);
-                    print(_amountController.text);
+                    Navigator.pop(context);
                   },
                   child: Text("Cancel")),
             ],
